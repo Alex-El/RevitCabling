3418fcb6df5b60a5290390ff3da2ad480eaa49ee
Aleksei Eltsov <eltsov.online@gmail.com>
2023-06-18 14:39:21 +0300

    Add project files.

---
+++ RevitCabling/Application.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitCabling
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Application : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Test AddIn", "Halo 2022!");
+
+            return Result.Succeeded;
+        }
+    }
+}
